fcdb3cccdcae3dcc24111425493733ae787f1664
Viktor Eisenstadt <vsa85@gmx.de>
2018-05-11 21:52:36 +0200

    add waapi animate example

---
+++ example03-waapi-animate/waapi.js
@@ -0,0 +1,15 @@
+var test = document.querySelector('#waapi-test');
+// Keyframes-Array
+var keyframes = [
+  { opacity: 0.9, width: '75px', left: '150px' },
+  { opacity: 0.6, width: '50px', left: '275px' },
+  { opacity: 0.3, width: '25px', left: '400px' },
+  { opacity: 0, width: '0', left: '525px' }
+];
+// Konfigurations-Array
+var config = {
+  iterations: Infinity,
+  duration: 700,
+  easing: 'ease-in'
+}
+test.animate(keyframes, 1500);
